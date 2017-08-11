e283d9f47491bc63ef7522746736d5b4c4640d4c
Erico Silva <augusto.ericosilva@gmail.com>
2017-08-11 15:02:42 -0300

    Add platforms radio button

--- app/src/main/java/com/couchbros/crimenet/EditProfileActivity.kt
+++ app/src/main/java/com/couchbros/crimenet/EditProfileActivity.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.util.Log
+import android.widget.ArrayAdapter
 import android.widget.Toast
 import com.couchbros.crimenet.enums.Platform
 import com.couchbros.crimenet.enums.PlayerAlignment
